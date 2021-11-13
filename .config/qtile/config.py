876c854a5239e4e977d1b5719e5c90086e7fe9d8
namku <syak7771@gmail.com>
2021-11-13 14:03:15 +0100

    Change default terminal in qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -35,7 +35,8 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
 mod = "mod4"
-terminal = guess_terminal()
+#terminal = guess_terminal()
+terminal = "kitty"
 
 keys = [
     # Switch between windows
