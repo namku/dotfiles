f74739080715a2e01d0a6cce9ea0524984339d46
namku <syak7771@gmail.com>
2022-03-24 13:40:46 +0100

    Fix for loop in qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -102,15 +102,15 @@ for i in range(len(group_names)):
             label=group_labels[i],
         ))
 
-for i in groups:
+for i in range(len(groups)):
     keys.extend([
-        Key([mod], i.name, lazy.group[i.name].toscreen()),
+        Key([mod], groups[i].name, lazy.group[groups[i].name].toscreen()),
         Key([mod], "Tab", lazy.screen.next_group()),
         Key([mod, "shift" ], "Tab", lazy.screen.prev_group()),
         Key(["mod1"], "Tab", lazy.screen.next_group()),
         Key(["mod1", "shift"], "Tab", lazy.screen.prev_group()),
 
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name) , lazy.group[i.name].toscreen()),
+        Key([mod, "shift"], groups[i].name, lazy.window.togroup(groups[i].name) , lazy.group[groups[i].name].toscreen()),
     ])
 
 layouts = [
