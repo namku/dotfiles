403833c5fe5ce3d546fd71cca06d62effb0dc4af
namku <syak7771@gmail.com>
2024-01-24 18:54:40 +0100

    Update wallpaper retro

--- .config/hypr/scripts/changeTheme.sh
+++ .config/hypr/scripts/changeTheme.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#chosen=$(printf "Power Off\nRestart" | wofi --conf ~/.config/hypr/wofi/config --style ~/.config/hypr/wofi/style.    css --color ~/.config/hypr/wofi/colors -dmenu -i)
-#
-
 chosen=$(printf "Retro\nPink" | wofi -dmen -i --conf ~/.config/hypr/wofi/config --style ~/.config/hypr/wofi/style.css --color ~/.config/hypr/wofi/colors)
 
 case "$chosen" in
@@ -10,8 +7,8 @@ case "$chosen" in
     cp /home/nham/.config/hypr/waybar/style_retro.css /home/nham/.config/hypr/waybar/style.css
     killall -9 waybar && /home/nham/.config/hypr/scripts/statusbar &
     sed -i 's/col.active_border.*$/col.active_border = rgba(f5f5dcee) rgba(626258ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
-    sed -i 's/swaybg.*$/swaybg --output "*" --mode fill --image ~\/.config\/hypr\/wallpapers\/retro.jpg \&/g' /home/nham/.config/hypr/scripts/startup
-    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/retro.jpg
+    sed -i 's/swaybg.*$/swaybg --output "*" --mode fill --image ~\/.config\/hypr\/wallpapers\/sheet.jpg \&/g' /home/nham/.config/hypr/scripts/startup
+    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/sheet.jpg
     ;;
   "Pink") 
     cp /home/nham/.config/hypr/waybar/style_pink.css /home/nham/.config/hypr/waybar/style.css
