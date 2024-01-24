a5fc7fe5e706c694014f735ba7f125939aeeec75
namku <syak7771@gmail.com>
2024-01-24 14:50:47 +0100

    Add themes in changethemes.sh

--- .config/hypr/scripts/changeTheme.sh
+++ .config/hypr/scripts/changeTheme.sh
@@ -7,14 +7,16 @@ chosen=$(printf "Retro\nPink" | wofi -dmen -i --conf ~/.config/hypr/wofi/config 
 
 case "$chosen" in
   "Retro") 
+    cp /home/nham/.config/hypr/waybar/style_retro.css /home/nham/.config/hypr/waybar/style.css
+    killall -9 waybar && /home/nham/.config/hypr/scripts/statusbar &
+    sed -i 's/col.active_border.*$/col.active_border = rgba(f5f5dcee) rgba(626258ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
     killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/retro.jpg
-    sed -i 's/col.active_border.*$/col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
-    killall -9 waybar
-    /home/nham/.config/hypr/scripts/statusbar &
     ;;
   "Pink") 
-    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/wallpaper.png
-    sed -i 's/col.active_border.*$/col.active_border = rgba(f5f5dcee) rgba(626258ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
+    cp /home/nham/.config/hypr/waybar/style_pink.css /home/nham/.config/hypr/waybar/style.css
+    killall -9 waybar && /home/nham/.config/hypr/scripts/statusbar &
+    sed -i 's/col.active_border.*$/col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
+    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/wallpaper.png 
     ;;
   *) exit 1
     ;;
