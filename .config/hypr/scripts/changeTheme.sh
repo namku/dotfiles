efe8ab25d0c70fd9767623a7b112802e8d2d0b21
namku <syak7771@gmail.com>
2024-01-24 14:03:48 +0100

    Add changeTheme.sh

--- .config/hypr/scripts/changeTheme.sh
+++ .config/hypr/scripts/changeTheme.sh
@@ -3,10 +3,17 @@
 #chosen=$(printf "Power Off\nRestart" | wofi --conf ~/.config/hypr/wofi/config --style ~/.config/hypr/wofi/style.    css --color ~/.config/hypr/wofi/colors -dmenu -i)
 #
 
-chosen=$(printf "Orig\nTest" | wofi -dmen -i --conf ~/.config/hypr/wofi/config --style ~/.config/hypr/wofi/style.css --color ~/.config/hypr/wofi/colors)
+chosen=$(printf "Retro\nPink" | wofi -dmen -i --conf ~/.config/hypr/wofi/config --style ~/.config/hypr/wofi/style.css --color ~/.config/hypr/wofi/colors)
 
 case "$chosen" in
-  "Orig") killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/01-wall.jpg ;;
-  "Test") killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/wallpaper.png ;;
-  *) exit 1 ;;
+  "Retro") 
+    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/retro.jpg
+    sed -i 's/col.active_border.*$/col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
+    ;;
+  "Pink") 
+    killall swaybg && swaybg --output '*' --mode fill --image ~/.config/hypr/wallpapers/wallpaper.png
+    sed -i 's/col.active_border.*$/col.active_border = rgba(f5f5dcee) rgba(626258ee) 45deg/g' /home/nham/.config/hypr/hyprland.conf
+    ;;
+  *) exit 1
+    ;;
 esac
